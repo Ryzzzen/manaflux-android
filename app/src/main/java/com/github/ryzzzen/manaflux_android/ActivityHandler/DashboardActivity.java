0617eb772b0da6123a6850279cf8e897016c1b97
Jakub Kot <42355410+kko7@users.noreply.github.com>
2018-11-13 08:54:13 +0100

    Added TextView
    NOT TESTED

--- app/src/main/java/com/github/ryzzzen/manaflux_android/ActivityHandler/DashboardActivity.java
+++ app/src/main/java/com/github/ryzzzen/manaflux_android/ActivityHandler/DashboardActivity.java
@@ -7,14 +7,14 @@ import com.github.ryzzzen.manaflux_android.ConnectionHandler.HttpGetRequest;
 import com.github.ryzzzen.manaflux_android.R;
 
 public class DashboardActivity extends Activity {
-
+    TextView statusLabel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         String ipAddress = getIntent().getStringExtra("ip_address");
-        HttpGetRequest task = new HttpGetRequest();
-
+        statusLabel = findViewById(R.id.buttonAction);
+        HttpGetRequest task = new HttpGetRequest(statusLabel);
         task.execute("http://" + ipAddress + ":4500/summoner");
 
     }
