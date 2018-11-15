0a12478ab7a0ff7425ca8bf9849c2e4e4f64d158
kko7 <j4kub.kko7@gmail.com>
2018-11-15 07:48:16 +0100

    - Asking for Permissions on start  - Removed TextView from Dashboard

--- app/src/main/java/com/github/ryzzzen/manaflux_android/ActivityHandler/DashboardActivity.java
+++ app/src/main/java/com/github/ryzzzen/manaflux_android/ActivityHandler/DashboardActivity.java
@@ -2,28 +2,19 @@ package com.github.ryzzzen.manaflux_android.ActivityHandler;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.github.ryzzzen.manaflux_android.ConnectionHandler.HttpGetRequest;
 import com.github.ryzzzen.manaflux_android.R;
 
 public class DashboardActivity extends Activity {
-	public TextView nameView;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         String ipAddress = getIntent().getStringExtra("ip_address");
-		
-		nameView = findViewById(R.id.summonerNaem_view);
-        
         HttpGetRequest task = new HttpGetRequest();
         task.execute("http://" + ipAddress + ":4500/summoner");
 
     }
-	
-	public setName(String name) {
-		nameView.setText(name);
-	}
 }
