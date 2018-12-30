efd21e717991b4aea50a2aac678ada809ba4c2eb
kko7 <j4kub.kko7@gmail.com>
2018-12-30 22:16:54 +0100

    Improvements

--- app/src/main/java/com/github/kko7/manaflux_android/MainActivity.java
+++ app/src/main/java/com/github/kko7/manaflux_android/MainActivity.java
@@ -1,8 +1,12 @@
 package com.github.kko7.manaflux_android;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.github.kko7.manaflux_android.Helpers.PrefsHelper;
 import com.github.kko7.manaflux_android.UserInterface.LoadActivity;
@@ -19,14 +23,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void checkDevice() {
-        PrefsHelper prefsHelper = PrefsHelper.getInstance(this);
-        String deviceIP = prefsHelper.getString("deviceIP");
-        if (deviceIP == null || deviceIP.equals("")) {
-            intent = new Intent(MainActivity.this, SelectActivity.class);
-        } else {
-            intent = new Intent(MainActivity.this, LoadActivity.class);
+        try {
+            PrefsHelper prefsHelper = PrefsHelper.getInstance(this);
+            String deviceIP = prefsHelper.getString("deviceIP");
+            String deviceNAME = prefsHelper.getString("deviceNAME");
+
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                Log.d("MainActivity", "Camera perm = OK");
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 201);
+            }
+
+            if (deviceIP == null || deviceIP.equals("") || deviceNAME == null || deviceNAME.equals("")) {
+                intent = new Intent(MainActivity.this, SelectActivity.class);
+            } else {
+                intent = new Intent(MainActivity.this, LoadActivity.class);
+            }
+
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        startActivity(intent);
+
     }
 
 }
