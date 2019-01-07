92e8f168096ad6905e17bea39526fb658110c02a
kko7 <j4kub.kko7@gmail.com>
2019-01-07 11:07:30 +0100

    Read description
    ADDED
     - GifView [WIP]
     - Display errors on screen
    TODO
     - Add sendToken
     - Handle socket errors
     - Start implementing dashboard

--- app/src/main/java/com/github/kko7/manaflux_android/UserInterface/LoadActivity.java
+++ app/src/main/java/com/github/kko7/manaflux_android/UserInterface/LoadActivity.java
@@ -1,14 +1,18 @@
 package com.github.kko7.manaflux_android.UserInterface;
 
+import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.github.kko7.manaflux_android.Connection.HttpGet;
 import com.github.kko7.manaflux_android.Connection.HttpListener;
+import com.github.kko7.manaflux_android.CustomElements.GifView;
 import com.github.kko7.manaflux_android.Helpers.PrefsHelper;
 import com.github.kko7.manaflux_android.R;
 
@@ -24,9 +28,11 @@ public class LoadActivity extends AppCompatActivity implements HttpListener {
 
     private static final String TAG = "LoadActivity";
     PrefsHelper prefsHelper;
-    RelativeLayout layout;
-    TextView status;
+    RelativeLayout layout, error_layout;
+    TextView error_code, error_message;
+    Button refreshButton;
     String ip, name, token;
+    GifView loading_gif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +41,29 @@ public class LoadActivity extends AppCompatActivity implements HttpListener {
         Log.d(TAG, "onCreate: Started.");
         init();
         setBackground();
-        check();
+        checkAll();
     }
 
     private void init() {
         prefsHelper = PrefsHelper.getInstance(this);
         layout = findViewById(R.id.load_layout);
-        status = findViewById(R.id.status_text);
+        error_layout = findViewById(R.id.error_layout);
+        error_code = findViewById(R.id.text_code);
+        error_message = findViewById(R.id.text_message);
+        loading_gif = findViewById(R.id.loading_gif);
+        loading_gif.setGifImageResource(R.mipmap.loading);
+        refreshButton = findViewById(R.id.refresh_button);
         ip = prefsHelper.getString("deviceIP");
         name = prefsHelper.getString("deviceNAME");
         token = prefsHelper.getString("token");
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                checkAll();
+            }
+        });
     }
 
     private void setBackground() {
@@ -54,12 +72,13 @@ public class LoadActivity extends AppCompatActivity implements HttpListener {
         layout.setBackgroundResource(id);
     }
 
-    private void check() {
+    private void checkAll() {
+        loading_gif.setVisibility(View.VISIBLE);
+        error_layout.setVisibility(View.GONE);
         try {
             HttpGet httpGet = new HttpGet("http://" + ip + ":4500/summoner", this);
             httpGet.run();
         } catch (Exception e) {
-            status.setText(String.valueOf(e));
             e.printStackTrace();
             Log.d(TAG, e.getMessage());
         }
@@ -70,10 +89,13 @@ public class LoadActivity extends AppCompatActivity implements HttpListener {
         try {
             if (!response.isSuccessful()) {
                 runOnUiThread(new Runnable() {
+                    @SuppressLint("SetTextI18n")
                     @Override
                     public void run() {
-                        status.setText(response.toString());
-                        status.setVisibility(TextView.VISIBLE);
+                        error_layout.setVisibility(View.VISIBLE);
+                        loading_gif.setVisibility(View.GONE);
+                        error_code.setText("Code: " + response.code()); //English only
+                        error_message.setText("Message: " + response.message()); //English only
                     }
                 });
             } else {
@@ -91,7 +113,8 @@ public class LoadActivity extends AppCompatActivity implements HttpListener {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                status.setText(exception);
+                Log.d(TAG, exception);
+                //TODO display on screen
             }
         });
     }
