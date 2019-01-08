4d3428fa357eaa79ea747895340a89ee94748f6a
Jakub Kot <42355410+kko7@users.noreply.github.com>
2019-01-08 10:22:21 +0100

    Update HttpListener.java

--- app/src/main/java/com/github/kko7/manaflux_android/Connection/HttpListener.java
+++ app/src/main/java/com/github/kko7/manaflux_android/Connection/HttpListener.java
@@ -1,8 +1,9 @@
 package com.github.kko7.manaflux_android.Connection;
 
 import okhttp3.Response;
+import java.io.IOException;
 
 public interface HttpListener {
     void onResponse(Response response);
-    void onFailure(String exception);
+    void onFailure(IOException exception);
 }
