56ceb3dc2f0ce5a8ecf91b0262a7a3a3b669fe8f
kko7 <j4kub.kko7@gmail.com>
2019-01-25 16:37:22 +0100

    ChampionSelect [WIP]

---
+++ app/src/main/java/com/github/kko7/manaflux_android/UserInterface/ChampionSelect.java
@@ -0,0 +1,15 @@
+package com.github.kko7.manaflux_android.UserInterface;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.kko7.manaflux_android.R;
+
+public class ChampionSelect extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_champion_select);
+    }
+}
