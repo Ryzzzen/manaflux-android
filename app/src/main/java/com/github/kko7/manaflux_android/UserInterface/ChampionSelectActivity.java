25ae5e9ce0e1a7926d54ff7bd88d09eb9f02c210
kko7 <j4kub.kko7@gmail.com>
2019-01-27 16:50:41 +0100

    Added findViewById

--- app/src/main/java/com/github/kko7/manaflux_android/UserInterface/ChampionSelectActivity.java
+++ app/src/main/java/com/github/kko7/manaflux_android/UserInterface/ChampionSelectActivity.java
@@ -2,14 +2,29 @@ package com.github.kko7.manaflux_android.UserInterface;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Spinner;
 
+import com.github.kko7.manaflux_android.CustomElements.CircularImageView;
 import com.github.kko7.manaflux_android.R;
 
 public class ChampionSelectActivity extends AppCompatActivity {
+
+    private static final String TAG = ChampionSelectActivity.class.getSimpleName();
+    CircularImageView championImage;
+    Button spellButton1, spellButton2;
+    Spinner runes;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_champion_select);
+        Log.d(TAG, "onCreate: Started");
+
+        championImage = findViewById(R.id.champion_image);
+        spellButton1 = findViewById(R.id.spell_button1);
+        spellButton2 = findViewById(R.id.spell_button2);
+        runes = findViewById(R.id.runes_spinner);
     }
 }
