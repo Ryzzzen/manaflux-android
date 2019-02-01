f8354655d73269357405e82cae7844700b607123
kko7 <j4kub.kko7@gmail.com>
2019-02-01 20:38:02 +0100

    Removed placeholder

--- app/src/main/java/com/github/kko7/manaflux_android/UserInterface/Dashboard/ProfileFragment.java
+++ app/src/main/java/com/github/kko7/manaflux_android/UserInterface/Dashboard/ProfileFragment.java
@@ -5,9 +5,7 @@ import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.github.kko7.manaflux_android.Helpers.PrefsHelper;
 import com.github.kko7.manaflux_android.R;
 
 public class ProfileFragment extends android.support.v4.app.Fragment {
@@ -18,11 +16,5 @@ public class ProfileFragment extends android.support.v4.app.Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull final View view, Bundle savedInstanceState) {
-        TextView text = view.findViewById(R.id.textView2);
-        text.setText(PrefsHelper.getInstance(view.getContext()).getString("summonerName"));
     }
 }
