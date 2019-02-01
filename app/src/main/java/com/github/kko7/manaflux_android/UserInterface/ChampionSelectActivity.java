eeb85730c11df2564df5aa3feed2ebb71911a4ad
kko7 <j4kub.kko7@gmail.com>
2019-02-01 13:36:20 +0100

    Preparing champion select

--- app/src/main/java/com/github/kko7/manaflux_android/UserInterface/ChampionSelectActivity.java
+++ app/src/main/java/com/github/kko7/manaflux_android/UserInterface/ChampionSelectActivity.java
@@ -7,14 +7,17 @@ import android.widget.Button;
 import android.widget.Spinner;
 
 import com.github.kko7.manaflux_android.CustomElements.CircularImageView;
+import com.github.kko7.manaflux_android.Helpers.PrefsHelper;
 import com.github.kko7.manaflux_android.R;
 
 public class ChampionSelectActivity extends AppCompatActivity {
 
     private static final String TAG = ChampionSelectActivity.class.getSimpleName();
+    String ip;
     CircularImageView championImage;
     Button spellButton1, spellButton2;
-    Spinner runes;
+    Spinner runesSpinner, positionsSpinner;
+    PrefsHelper prefsHelper;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -22,9 +25,12 @@ public class ChampionSelectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_champion_select);
         Log.d(TAG, "onCreate: Started");
 
+        prefsHelper = PrefsHelper.getInstance(this);
         championImage = findViewById(R.id.champion_image);
         spellButton1 = findViewById(R.id.spell_button1);
         spellButton2 = findViewById(R.id.spell_button2);
-        runes = findViewById(R.id.runes_spinner);
+        runesSpinner = findViewById(R.id.runes_spinner);
+        ip = prefsHelper.getString("device-ip");
     }
+
 }
