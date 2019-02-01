9efbfda997c4149e388319e9b91e21b94d520b62
kko7 <j4kub.kko7@gmail.com>
2019-02-01 20:39:00 +0100

    ChampionSelectActivity [WiP]
    Most probably still broken

--- app/src/main/java/com/github/kko7/manaflux_android/UserInterface/ChampionSelectActivity.java
+++ app/src/main/java/com/github/kko7/manaflux_android/UserInterface/ChampionSelectActivity.java
@@ -1,23 +1,41 @@
 package com.github.kko7.manaflux_android.UserInterface;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Button;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-import com.github.kko7.manaflux_android.CustomElements.CircularImageView;
-import com.github.kko7.manaflux_android.Helpers.PrefsHelper;
+import com.github.kko7.manaflux_android.Connection.ApiClient;
+import com.github.kko7.manaflux_android.Connection.ApiData;
+import com.github.kko7.manaflux_android.Connection.ApiInterface;
+import com.github.kko7.manaflux_android.CustomElements.TextView;
 import com.github.kko7.manaflux_android.R;
+
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ChampionSelectActivity extends AppCompatActivity {
 
     private static final String TAG = ChampionSelectActivity.class.getSimpleName();
-    String ip;
-    CircularImageView championImage;
-    Button spellButton1, spellButton2;
-    Spinner runesSpinner, positionsSpinner;
-    PrefsHelper prefsHelper;
+    private String[] positions;
+    private Context context;
+    private TextView code;
+    private TextView error;
+    private RelativeLayout errorLayout;
+    private RelativeLayout layout;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,12 +43,98 @@ public class ChampionSelectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_champion_select);
         Log.d(TAG, "onCreate: Started");
 
-        prefsHelper = PrefsHelper.getInstance(this);
-        championImage = findViewById(R.id.champion_image);
-        spellButton1 = findViewById(R.id.spell_button1);
-        spellButton2 = findViewById(R.id.spell_button2);
-        runesSpinner = findViewById(R.id.runes_spinner);
-        ip = prefsHelper.getString("device-ip");
+        context = this;
+        code = findViewById(R.id.errorCode);
+        error = findViewById(R.id.error);
+        layout = findViewById(R.id.select_layout);
+        errorLayout = findViewById(R.id.error_layout);
+        start();
     }
 
+    private void start() {
+        layout.setVisibility(View.VISIBLE);
+        errorLayout.setVisibility(View.GONE);
+        final ApiInterface client = new ApiClient(this).getClient();
+        Call<ApiData> getPositions = client.getPositions();
+        getPositions.enqueue(new Callback<ApiData>() {
+            @Override
+            public void onResponse(@NonNull Call<ApiData> call, @NonNull Response<ApiData> response) {
+                ApiData data = response.body();
+                assert data != null;
+                if (response.isSuccessful() && data.getSuccess()) {
+                    positions = data.getPositions();
+                    initList();
+                } else {
+                    showError(data.getErrorCode(), data.getError());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ApiData> call, @NonNull Throwable throwable) {
+                showException(call, throwable);
+            }
+        });
+    }
+
+    private void initList() {
+        ArrayAdapter adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, positions);
+        Spinner positionsSpinner = findViewById(R.id.position_spinner);
+        positionsSpinner.setAdapter(adapter);
+        positionsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, final View v, final int position, long id) {
+                final ApiInterface client = new ApiClient(v.getContext()).getClient();
+                Call<ApiData> setPosition = client.setPosition(position);
+                setPosition.enqueue(new Callback<ApiData>() {
+                    @Override
+                    public void onResponse(@NonNull Call<ApiData> call, @NonNull Response<ApiData> response) {
+                        if (response.isSuccessful()) {
+                            Toast.makeText(v.getContext(), getString(R.string.champion_select_changed) + positions[position], Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(v.getContext(), getString(R.string.champion_select_error), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<ApiData> call, @NonNull Throwable t) {
+                        showException(call, t);
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                //TODO
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void showError(String message1, String message2) {
+        errorLayout.setVisibility(View.VISIBLE);
+        layout.setVisibility(View.GONE);
+        code.setText(message1);
+        error.setText(message2);
+    }
+
+    private void showException(final Call call, final Throwable throwable) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                String error1, error2;
+                if (throwable instanceof SocketTimeoutException) {
+                    error1 = "Connect timed out";
+                    error2 = "Host: " + call.request().url().host();
+                } else if (throwable instanceof UnknownHostException) {
+                    error1 = "Unable to resolve host";
+                    error2 = "Host: " + call.request().url();
+                } else {
+                    error1 = "Other exception";
+                    error2 = "Contact developer";
+                }
+                showError(error1, error2);
+            }
+        });
+    }
 }
