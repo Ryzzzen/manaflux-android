7f2985bb5354a00ee5b36fcd86bbd11303df33ea
kko7 <j4kub.kko7@gmail.com>
2019-02-02 14:45:51 +0100

    heartbeat: removed unused code, fixed name

--- app/src/main/java/com/github/kko7/manaflux_android/Connection/HeartbeatData.java
+++ app/src/main/java/com/github/kko7/manaflux_android/Connection/HeartbeatData.java
@@ -4,32 +4,18 @@ import com.google.gson.annotations.SerializedName;
 
 public class HeartbeatData {
 
-    @SerializedName("success")
-    private Boolean success;
-    @SerializedName("mame")
+    @SerializedName("name")
     private String championName;
-    @SerializedName("id")
-    private String championId;
     @SerializedName("img")
     private String championImg;
 
-    public HeartbeatData(Boolean success, String championName, String championId, String championImg) {
-        this.success = success;
+    public HeartbeatData(String championName, String championImg) {
         this.championName = championName;
-        this.championId = championId;
         this.championImg = championImg;
-    }
-
-    public Boolean getSuccess() {
-        return success;
     }
 
     public String getChampionName() {
         return championName;
-    }
-
-    public String getChampionId() {
-        return championId;
     }
 
     public String getChampionImg() {
