094b2d0b21f5d7961106475961e5ae629fa63160
kko7 <j4kub.kko7@gmail.com>
2019-03-17 00:51:33 +0100

    Use saved data

--- app/src/main/java/com/github/kko7/manaflux_android/UserInterface/Dashboard/ProfileFragment.java
+++ app/src/main/java/com/github/kko7/manaflux_android/UserInterface/Dashboard/ProfileFragment.java
@@ -9,22 +9,11 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.github.kko7.manaflux_android.Connection.ApiClient;
-import com.github.kko7.manaflux_android.Connection.ApiInterface;
 import com.github.kko7.manaflux_android.Helpers.PrefsHelper;
-import com.github.kko7.manaflux_android.Models.ApiData;
 import com.github.kko7.manaflux_android.R;
 import com.squareup.picasso.Picasso;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class ProfileFragment extends Fragment {
-
-    private ApiInterface client;
-    private String summonerIcon;
-    private String summonerDisplayName;
 
     public ProfileFragment() {
     }
@@ -37,11 +26,12 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull final View view, Bundle savedInstanceState) {
-        final String deviceIp = PrefsHelper.getInstance(view.getContext()).getString("device-ip");
+        final PrefsHelper prefsHelper = PrefsHelper.getInstance(view.getContext());
+        final String summonerDisplayName = prefsHelper.getString("summoner-name");
+        final String summonerIcon = prefsHelper.getString("summoner-icon");
+        final String deviceIp = prefsHelper.getString("device-ip");
         final ImageView summonerImage = view.findViewById(R.id.summonerImage);
         final TextView summonerName = view.findViewById(R.id.summonerName);
-        client = new ApiClient(view.getContext()).getClient();
-        getSummonerData();
         Picasso.get()
                 .load(summonerIcon
                         .replace("http://localhost:", deviceIp))
@@ -49,29 +39,5 @@ public class ProfileFragment extends Fragment {
                 .error(R.mipmap.champion_placeholder)
                 .into(summonerImage);
         summonerName.setText(summonerDisplayName);
-    }
-
-    private void getSummonerData() {
-        final Call<ApiData> summonerData = client.getSummoner();
-        summonerData.enqueue(new Callback<ApiData>() {
-            @Override
-            public void onResponse(@NonNull Call<ApiData> call,
-                                   @NonNull final Response<ApiData> response) {
-                assert response.body() != null;
-                if (response.isSuccessful() && response.body().getSuccess()) {
-                    summonerDisplayName = response.body().getSummonerName();
-                    summonerIcon = response.body().getSummonerIcon();
-                } else {
-                    summonerDisplayName = "Failed to get data";
-                    summonerIcon = null;
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<ApiData> call, @NonNull Throwable throwable) {
-                summonerDisplayName = "Failed to get data";
-                summonerIcon = null;
-            }
-        });
     }
 }
